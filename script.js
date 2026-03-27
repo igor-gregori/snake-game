3a9e95efefc8274ce11931600a267a41bb2e6f08
Igor Gregori <igsilvacarlos@gmail.com>
2026-03-27 12:35:03 -0300

    verify wall and show food

--- script.js
+++ script.js
@@ -8,18 +8,31 @@ let snake = [
 ];
 let direction = "right";
 let squareSize = 10;
+let food = {
+  x: Math.floor(Math.random() * (canvas.width / 10)) * 10,
+  y: Math.floor(Math.random() * (canvas.height / 10)) * 10,
+};
 
 window.addEventListener("keydown", function (event) {
-  // TODO: can i trade rigth by left?
-  if (event.key === "ArrowUp") direction = "up";
-  if (event.key === "ArrowDown") direction = "down";
-  if (event.key === "ArrowLeft") direction = "left";
-  if (event.key === "ArrowRight") direction = "right";
+  switch (event.key) {
+    case "ArrowUp":
+      if (direction !== "down") direction = "up";
+      break;
+    case "ArrowDown":
+      if (direction !== "up") direction = "down";
+      break;
+    case "ArrowLeft":
+      if (direction !== "right") direction = "left";
+      break;
+    case "ArrowRight":
+      if (direction !== "left") direction = "right";
+      break;
+  }
 });
 
 const intervalID = setInterval(() => {
   draw();
-}, 500);
+}, 100);
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -27,10 +40,24 @@ function draw() {
     ctx.fillStyle = "white";
     ctx.fillRect(square.x, square.y, squareSize, squareSize);
   }
-  // verify wall
+  ctx.fillStyle = "green";
+  ctx.fillRect(food.x, food.y, squareSize, squareSize);
+  verifyWall();
   // verify food
   // verify snake body
   addSquare();
+}
+
+function verifyWall() {
+  const head = snake[snake.length - 1];
+  if (head.x < 0 || head.x > canvas.width) {
+    clearInterval(intervalID);
+    alert("game over");
+  }
+  if (head.y < 0 || head.y > canvas.height) {
+    clearInterval(intervalID);
+    alert("game over");
+  }
 }
 
 function addSquare() {
